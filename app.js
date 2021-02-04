06af43fd03552f21ffb52cc1a9cdd0d8d720e288
iamsaif <saifulhasan580@gmail.com>
2021-02-04 18:21:33 +0530

    connecting mongoose

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 //jshint esversion:6
 const express=require("express");
 const ejs=require("ejs");
+const path=require("path")
+const mongoose=require("mongoose")
 
 const app=express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost:27017/authDEMO', {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false })
+    .then(()=>{
+        console.log("Connection establish");
+    })
+    .catch(err=>{
+        console.log("Something went wrong");
+        console.log(err);
+    })
 
 app.get('/',(req,res)=>{
     res.render("home.ejs")
