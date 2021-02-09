ddfcf063c1a65ef6c2aea302512c0f1fe4ebef7d
iamsaif <saifulhasan580@gmail.com>
2021-02-10 03:06:08 +0530

    asd

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://immense-beyond-68906.herokuapp.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -100,7 +100,7 @@ app.get('/auth/google',
 app.get('/auth/google/secrets', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('https://immense-beyond-68906.herokuapp.com/auth/google/secrets');
+    res.redirect('/secrets');
 });
 
 app.get("/auth/facebook", passport.authenticate("facebook"));
